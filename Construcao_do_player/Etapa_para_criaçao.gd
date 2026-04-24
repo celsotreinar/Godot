2ada114507df15767fd15edccb1050227eea8b8c
celso_alves <celso4.3alves@gmail.com>
2026-04-24 12:29:44 -0300

    melhoria da alteracao

---
+++ Construcao_do_player/Etapa_para_criaçao.gd
@@ -0,0 +1,16 @@
+# 1ª etapa para criação do player
+# para criar um personagem jogável em um jogo 2D usando o Godot Engine, você pode seguir os seguintes passos:
+# 1. Crie um novo nó do tipo CharacterBody2D para representar o personagem. 
+characterbody2d 
+
+# 2ª etapa para criação do player
+# Adicione um nó filho do tipo CollisionShape2D para definir a forma de colisão do personagem.
+CollisionShape2D 
+
+# 3ª etapa para criação do player
+# Adicione um nó filho do tipo Sprite2D para representar a aparência visual do personagem.
+Sprite2D 
+
+# 4ª etapa para criação do player
+# Adicione um nó filho do tipo AnimatedSprite2D para permitir animações do personagem.
+AnimatedSprite2D
